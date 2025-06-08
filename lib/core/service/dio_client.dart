b8456e881abcbd3c9ace3bdb84fd384737efe783
mefulnh <153336769+mefulnh@users.noreply.github.com>
2025-06-08 21:46:53 +0700

    Update dio_client.dart

--- lib/core/service/dio_client.dart
+++ lib/core/service/dio_client.dart
@@ -5,7 +5,7 @@ import '../utils/storage_helper.dart';
 class DioClient {
   static final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: 'http://10.0.2.2:8000/api',
+      baseUrl: 'http://127.0.0.1:8000/api',
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       headers: {
